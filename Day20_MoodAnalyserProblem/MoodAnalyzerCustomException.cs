12ccc16eb471b00909fbaac6fcd86eaf16df2f83
Tejassonar <sonartejas908@gmail.com>
2021-10-15 13:58:40 +0530

    Added Custom Exception class

---
+++ Day20_MoodAnalyserProblem/MoodAnalyzerCustomException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day20_MoodAnalyserProblem
+{
+    public class MoodAnalyzerCustomException : Exception
+    {
+        public enum ExceptionType
+        {
+            NULL_MESSAGE,
+            EMPTY_MESSAGE,
+            NO_SUCH_FIELD,
+            NO_SUCH_METHOD,
+            NO_SUCH_CLASS,
+            OBJECT_CREATION_ISSUE
+        }
+        private readonly ExceptionType type;
+
+        public MoodAnalyzerCustomException(ExceptionType Type, string message) : base(message)
+        {
+            this.type = Type;
+        }
+    }
+}
