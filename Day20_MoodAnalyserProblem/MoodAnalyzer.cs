6dfc4734a5861e826a1146930f9f9eabc2ffebc1
Tejassonar <sonartejas908@gmail.com>
2021-10-15 14:00:30 +0530

    Added MoodAnalyzer class

---
+++ Day20_MoodAnalyserProblem/MoodAnalyzer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day20_MoodAnalyserProblem
+{
+    public class MoodAnalyzer
+    {
+        private string message;
+        public MoodAnalyzer(string message)
+        {
+            this.message = message;
+        }
+        public MoodAnalyzer()
+        {
+
+        }
+        public string analyseMood()
+        {
+            try
+            {
+                if (this.message.Equals(string.Empty))
+                {
+                    throw new MoodAnalyzerCustomException(MoodAnalyzerCustomException.ExceptionType.EMPTY_MESSAGE, "Mood should not be empty");
+
+                }
+                if (this.message.Contains("sad"))
+                    return "SAD";
+                else
+                    return "HAPPY";
+            }
+            catch (NullReferenceException)
+            {
+                throw new MoodAnalyzerCustomException(MoodAnalyzerCustomException.ExceptionType.NULL_MESSAGE, "Mood should not be null");
+
+            }
+        }
+    }
+}
